5e2118d0e98b84417e695bffd18fbe5452e214f2
tahsin <tahsinbd.ahmed1@gmail.com>
2023-11-13 17:06:39 +0600

    Ui updated and DOM added

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 let computer =0;
 let you = 0;
+const body = document.querySelector("body");
+const newDiv = document.createElement('div');
+body.append(newDiv);
+let flag = true;
+const score = document.createElement('p');
+newDiv.append(score);
+const allButton = document.querySelectorAll("input");
+allButton.forEach(button => {
+    button.addEventListener('click',()=>{
+        var cs = getComputerChoice();
+        var ps = button.value;
+        let para = document.createElement('p');
+        if(flag) playRound(ps,cs,para);
+        if(flag) score.innerText = "you: " + you + " and " + "Computer: "+computer;
+        newDiv.append(para);
+        if(computer===5 && flag) {
+            para.innerText = "Computer won, reload to play again!"
+            flag = false;
+            newDiv.append(para);
+        }
+        if(you===5 && flag)
+        {
+            para.innerText = "you won, reload to play again!";
+            flag = false;
+            newDiv.append(para);
+        }
+        
+    })
+})
 
 function getComputerChoice () {
     let choices = ["rock","paper","scissor"];
@@ -7,44 +36,33 @@ function getComputerChoice () {
     return choices[index];
 }
 
-function playRound (playerSelection,computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    if(playerSelection===computerSelection) document.write("match tied");
+function playRound (playerSelection,computerSelection,para) {
+    
+
+
+    if(playerSelection===computerSelection) para.innerText = "match tied";
 
     else if(playerSelection==="rock" && computerSelection==="paper")
     {
-        document.write("you chose " + playerSelection + " and " + " computer chose " + computerSelection + " Computer won");
+        para.innerText = "you chose " + playerSelection + " and " + " computer chose " + computerSelection + " Computer won";
         computer++;
     }
     else if(playerSelection==="paper" && computerSelection==="scissor")
     {
-        document.write("you chose " + playerSelection + " and " + " computer chose " + computerSelection + " Computer won");
+        para.innerText = "you chose " + playerSelection + " and " + " computer chose " + computerSelection + " Computer won";
         computer++;
     }
     else if(playerSelection==="scissor" && computerSelection==="rock")
     {
-        document.write("you chose " + playerSelection + " and " + " computer chose " + computerSelection + " Computer won");
+        para.innerText = "you chose " + playerSelection + " and " + " computer chose " + computerSelection + " Computer won";
         computer++;
     }
     else
     {
-        document.write("you chose " + playerSelection + " and " + "computer chose " + computerSelection + " You won");
+        para.innerText = "you chose " + playerSelection + " and " + "computer chose " + computerSelection + " You won";
         you++;
     }
-    document.write("<br>");
-}
-
-
-function game()
-{
-    for(let i=0;i<5;i++)
-    {
-        var cs = getComputerChoice();
-        var ps = prompt("what is your move?");
-        playRound(ps,cs);
-        document.write( "<br>" + "Computer: " + computer + "    " + "You: " +you);
-        document.write("<br>");
-    }
+    
 }
 
 
